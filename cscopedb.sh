92d5fdea88d28e4d5127482fb9733c36478f0025
Vishwa Pravin <lordvishwa123@gmail.com>
2023-03-11 10:00:51 +0530

    cscopedb modify

--- cscopedb.sh
+++ cscopedb.sh
@@ -4,9 +4,11 @@ OPENSSL_DIR=$HOME/Dev/tls/openssl
 SSLDUMP_DIR=$HOME/Dev/tls/ssldump
 
 rm cscope.*
-find $OPENSSL_DIR/ssl -name '*.c' -or -name '*.h' >> cscope.files
-find $OPENSSL_DIR/crypto -name '*.c' -or -name '*.h' >> cscope.files
-find $OPENSSL_DIR/include -name '*.c' -or -name '*.h' >> cscope.files
+if [[ $1 == "openssl" ]]; then
+	find $OPENSSL_DIR/ssl -name '*.c' -or -name '*.h' >> cscope.files
+	find $OPENSSL_DIR/crypto -name '*.c' -or -name '*.h' >> cscope.files
+	find $OPENSSL_DIR/include -name '*.c' -or -name '*.h' >> cscope.files
+fi
 find $SSLDUMP_DIR/ -name '*.c' -or -name '*.h' >> cscope.files
 
 cscope -b -q -k -F cscope.files
